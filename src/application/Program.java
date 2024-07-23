58ad96e6c801e738c0f5ce565889d30e02562437
Michael Rafael de Andrade <realteceinformatica@gmail.com>
2024-07-22 23:14:47 -0300

    exercicio resolvido

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,42 @@
 package application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Program {
 
 	public static void main(String[] args) {
 		// exercicio_MatrizAcimaDiagonal
 		
-
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		int n;
+		
+		System.out.print("Qual a ordem da matriz? ");
+		n = sc.nextInt();
+		
+		int[][] matriz = new int[n][n];
+		
+		for(int i = 0; i < matriz.length; i++) {
+			for(int j = 0; j < matriz[i].length; j++) {
+				System.out.printf("Elemento [%d,%d]: ",i,j);
+				matriz[i][j] = sc.nextInt();
+			}
+		}
+		
+		int somaEDiagonal = 0;
+		
+		for(int i = 0; i < matriz.length; i++) {
+			for(int j = 0; j < matriz[i].length; j++) {
+				if(j > i) {
+					somaEDiagonal += matriz[i][j];
+				}
+			}
+		}
+		System.out.printf("SOMA DOS ELEMENTOS ACIMA DA DIAGONAL PRINCIPAL = %d",somaEDiagonal);
+		
+		sc.close();
 	}
 
 }
